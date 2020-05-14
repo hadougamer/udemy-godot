d57d92d6303e296ac24ef8e967bc83e52f0470e1
Alexandre Conrado <conrado@nasc.io>
2020-05-14 13:33:21 -0300

    Moving the tank

--- Rouge Tank/scripts/tank.gd
+++ Rouge Tank/scripts/tank.gd
@@ -1,11 +1,25 @@
 extends KinematicBody2D
 
-# Called when the node enters the scene tree for the first time.
+var speed=200
+
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# Multiply for delta to convert px per frame to px per second
-	translate( Vector2(-100,-50) * delta )
+	
+	var dir_x=0;
+	var dir_y=0;
+	
+	if Input.is_action_pressed("ui_right"):
+		dir_x+=1
+	elif Input.is_action_pressed("ui_left"):
+		dir_x-=1
+		
+	if Input.is_action_pressed("ui_up"):
+		dir_y-=1
+	elif Input.is_action_pressed("ui_down"):
+		dir_y+=1
+	
+	translate( Vector2(dir_x, dir_y) * delta * speed )
+	
 	pass
