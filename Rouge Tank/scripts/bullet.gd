5c99f06af6a373e806da9fc91cacd7e08a564a10
Alexandre Conrado <conrado@nasc.io>
2020-05-15 21:27:30 -0300

    Bullet control

--- Rouge Tank/scripts/bullet.gd
+++ Rouge Tank/scripts/bullet.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 var vel = 250
-var dir = Vector2(0,-1)
+# set_get sets a callback function every time the
+# variable is modified
+var dir = Vector2(0,-1) setget set_dir
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,3 +19,8 @@ func _process(delta):
 func _on_notifier_screen_exited():
 	# Destroy this object (bullet)
 	queue_free();
+
+func set_dir(val):
+	dir = val
+	# Updates the bullet angle
+	rotation = dir.angle()
--- Rouge Tank/scripts/tank.gd
+++ Rouge Tank/scripts/tank.gd
@@ -26,17 +26,22 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_shoot"):
 		# Get the number of bullets (on cannon bullet group)
 		# limit the number of bullets to max three (3)
-		if get_tree().get_nodes_in_group("cannon_bullet").size() < 3:
+		if get_tree().get_nodes_in_group("cannon_bullet").size() < 9:
 			
 			# Instances bullet
 			var bullet = pre_bullet.instance()
 			# Put on Muzzle position
 			bullet.global_position=$barrel/muzzle.global_position
 			
+			# Modify the bullet rirection (to rotate)
+			bullet.dir = Vector2( cos(rotation), sin(rotation) ).normalized()
+			
 			#attach to parent
 			$"../".add_child(bullet)
 			##get_parent().add_child(bullet);
-			
+
+	look_at(get_global_mouse_position())
+				
 	translate( Vector2(dir_x, dir_y) * delta * speed )
 	
 	pass
