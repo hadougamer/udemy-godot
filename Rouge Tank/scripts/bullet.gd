d8e170f4548b7b2e8522312452ea4034f51c0b72
Alexandre Conrado <conrado@nasc.io>
2020-05-21 09:35:47 -0300

    Animation destruct bullet, yeld to wait the animation finish signal

--- Rouge Tank/scripts/bullet.gd
+++ Rouge Tank/scripts/bullet.gd
@@ -11,6 +11,9 @@ var dir = Vector2(0,-1) setget set_dir
 # set when object is readt
 onready var init_pos = global_position
 
+# Bullet live status
+var live = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -19,11 +22,27 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	# Destriy bullets with pos difference greater than MAX_DIST
-	if global_position.distance_to(init_pos) >= MAX_DIST:
-		queue_free()
-	
-	translate( dir *  vel * delta )
+	if live:
+		# Destroy bullets with pos difference greater than MAX_DIST
+		if global_position.distance_to(init_pos) >= MAX_DIST:
+			$smoke.emitting = false # stops the particle
+			live = false
+			$sprite.visible = false
+			$anim_self_destruction.play("explode")
+			
+			# Turns the colisions off
+			monitoring=false
+			monitorable=false
+			
+			# Waits the anim finished signal before continue
+			yield($anim_self_destruction, "animation_finished")
+			
+			# destroy the bullet
+			queue_free()
+				
+		translate( dir *  vel * delta )
+		
+		
 
 
 # When the bullet go out the screen
