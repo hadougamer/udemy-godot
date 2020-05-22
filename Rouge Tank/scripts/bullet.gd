444fce68a3b2798e213807aa2c4028d9ab1f8435
Alexandre Conrado <conrado@nasc.io>
2020-05-22 09:41:34 -0300

    Using 'on_entered_body' listener and layer configs to perform collisions between bullet and stage elements

--- Rouge Tank/scripts/bullet.gd
+++ Rouge Tank/scripts/bullet.gd
@@ -25,25 +25,9 @@ func _process(delta):
 	if live:
 		# Destroy bullets with pos difference greater than MAX_DIST
 		if global_position.distance_to(init_pos) >= MAX_DIST:
-			$smoke.emitting = false # stops the particle
-			live = false
-			$sprite.visible = false
-			$anim_self_destruction.play("explode")
-			
-			# Turns the colisions off
-			monitoring=false
-			monitorable=false
-			
-			# Waits the anim finished signal before continue
-			yield($anim_self_destruction, "animation_finished")
-			
-			# destroy the bullet
-			queue_free()
+			autodestroy()
 				
 		translate( dir *  vel * delta )
-		
-		
-
 
 # When the bullet go out the screen
 func _on_notifier_screen_exited():
@@ -54,3 +38,27 @@ func set_dir(val):
 	dir = val
 	# Updates the bullet angle
 	rotation = dir.angle()
+
+
+func _on_Bullet_body_entered(body):
+	# Layer 4 = Wall
+	if body.collision_layer == 4:
+		print("destruct")
+		autodestroy()
+		
+# Destruct the bullet (and its effects)
+func autodestroy():
+	$smoke.emitting = false # stops the particle
+	live = false
+	$sprite.visible = false
+	$anim_self_destruction.play("explode")
+	
+	# Turns the colisions off
+	monitoring=false
+	monitorable=false
+	
+	# Waits the anim finished signal before continue
+	yield($anim_self_destruction, "animation_finished")
+	
+	# destroy the bullet
+	queue_free()
